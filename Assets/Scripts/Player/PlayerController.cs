42b862d0263f9f4196864d1732459462e60a6dee
AnthonyMolle <67618831+AnthonyMolle@users.noreply.github.com>
2023-01-24 02:39:50 -0800

    okay this is buggy as hell but its a start lol

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,27 +7,31 @@ public class PlayerController : MonoBehaviour
        [SerializeField] Camera cam;
        [SerializeField] Rigidbody rb;
        [SerializeField] float mouseSensitivity = 1;
-       [SerializeField] float moveSpeed;
-       [SerializeField] float jumpForce;
+       [SerializeField] float moveSpeed = 10;
+       [SerializeField] float jumpForce = 10;
 
         float xRotation = 0f;
 
        private void Awake() 
        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
+          Cursor.lockState = CursorLockMode.Locked;
+          Cursor.visible = false;
        }
 
        private void Update() 
        {
-            var rotationAmount = new Vector3(0, 1, 0) * Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            gameObject.transform.Rotate(rotationAmount, Space.Self);
+          var rotationAmount = new Vector3(0, 1, 0) * Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+          gameObject.transform.Rotate(rotationAmount, Space.Self);
 
-            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-            xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+          float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+          xRotation -= mouseY;
+          xRotation = Mathf.Clamp(xRotation, -80f, 80f);
 
-            cam.gameObject.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+          cam.gameObject.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+          var horizontalMovement = new Vector3(1, 0, 0) * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+          gameObject.transform.localPosition = horizontalMovement;
 
        }
 }
