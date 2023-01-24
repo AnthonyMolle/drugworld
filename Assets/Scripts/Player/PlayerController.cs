244424413dac57b31879c2e9df0a549943cd6c73
AnthonyMolle <67618831+AnthonyMolle@users.noreply.github.com>
2023-01-24 02:25:56 -0800

    player can look around ayeeee

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+       [SerializeField] Camera cam;
+       [SerializeField] Rigidbody rb;
+       [SerializeField] float mouseSensitivity = 1;
+       [SerializeField] float moveSpeed;
+       [SerializeField] float jumpForce;
+
+        float xRotation = 0f;
+
+       private void Awake() 
+       {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+       }
+
+       private void Update() 
+       {
+            var rotationAmount = new Vector3(0, 1, 0) * Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            gameObject.transform.Rotate(rotationAmount, Space.Self);
+
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+            cam.gameObject.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+       }
+}
