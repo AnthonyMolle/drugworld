498aa96993240b91b508b0c11a2ab7719076ef0a
AnthonyMolle <67618831+AnthonyMolle@users.noreply.github.com>
2023-02-01 02:52:19 -0800

    a lot more greyboxing

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -26,8 +26,8 @@ public class PlayerController : MonoBehaviour
 
        private void Awake() 
        {
-          Cursor.lockState = CursorLockMode.Locked;
           Cursor.visible = false;
+          Cursor.lockState = CursorLockMode.Locked;
        }
 
        private void Update() 
